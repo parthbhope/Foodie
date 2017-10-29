cbe2308e33825ed9d4e6604fb9561278c5072400
Ashutosh <ashutoshgajre98@gmail.com>
2017-10-29 16:56:32 +0530

    bas yuhi

--- app/src/main/java/javaproject/foodie/MenuActivity.java
+++ app/src/main/java/javaproject/foodie/MenuActivity.java
@@ -1,13 +1,53 @@
 package javaproject.foodie;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.support.design.*;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle; //The button
+
+    private Toolbar mToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        //setContentView(R.layout.activity_menu);
+        setContentView(R.layout.navigation_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        // method invoked only when the actionBar is not null.
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
